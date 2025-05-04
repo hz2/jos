0c2ce322bd52f5a63ec616876258e1b119db3347
jsondevers <dev.json2@gmail.com>
2025-05-04 12:05:19 -0700

    trying to reconfig testing

--- src/lib.rs
+++ src/lib.rs
@@ -5,6 +5,13 @@
 
 use core::panic::PanicInfo;
 
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+#[repr(u32)]
+pub enum QemuExitCode {
+    Success = 0x10,
+    Failed = 0x11,
+}
+
 pub trait Testable {
     fn run(&self) -> ();
 }
@@ -17,6 +24,15 @@ where
         serial_print!("{}...\t", core::any::type_name::<T>());
         self();
         serial_println!("[ok]");
+    }
+}
+
+pub fn exit_qemu(exit_code: QemuExitCode) {
+    use x86_64::instructions::port::Port;
+
+    unsafe {
+        let mut port = Port::new(0xf4);
+        port.write(exit_code as u32);
     }
 }
 
